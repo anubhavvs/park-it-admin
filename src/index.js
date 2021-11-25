4b5e1ec6e1baead76a273056a8654193221f2f1b
Anubhav saha <anubhavvsaha2001@gmail.com>
2021-11-26 03:25:40 +0530

    :heavy_plus_sign: added notistick

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { SnackbarProvider } from 'notistack';
 import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SnackbarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SnackbarProvider>
   </Provider>,
   document.getElementById('root')
 );
