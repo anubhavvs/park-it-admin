879b0032557f4fdf00a6e0ff87925ea4a06cc881
Anubhav saha <anubhavvsaha2001@gmail.com>
2021-11-26 03:19:53 +0530

    :building_construction: website layout

---
+++ src/components/DashboardLayout.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { styled } from '@mui/styles';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  display: 'flex',
+  height: '100%',
+  overflow: 'hidden',
+  width: '100%'
+}));
+
+const DashboardLayoutWrapper = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  overflow: 'hidden',
+  paddingTop: 64,
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: 256
+  }
+}));
+
+const DashboardLayoutContainer = styled('div')({
+  display: 'flex',
+  flex: '1 1 auto',
+  overflow: 'hidden'
+});
+
+const DashboardLayoutContent = styled('div')({
+  flex: '1 1 auto',
+  height: '100%',
+  overflow: 'auto'
+});
+
+const DashboardLayout = () => {
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+  return (
+    <DashboardLayoutRoot>
+      <Navbar onMobileNavOpen={() => setMobileNavOpen(true)} isMobileNavOpen={isMobileNavOpen} />
+      <Sidebar onMobileClose={() => setMobileNavOpen(false)} openMobile={isMobileNavOpen} />
+      <DashboardLayoutWrapper>
+        <DashboardLayoutContainer>
+          <DashboardLayoutContent>
+            <Outlet />
+          </DashboardLayoutContent>
+        </DashboardLayoutContainer>
+      </DashboardLayoutWrapper>
+    </DashboardLayoutRoot>
+  );
+};
+
+export default DashboardLayout;
