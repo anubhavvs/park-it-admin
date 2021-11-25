575b2ca018a115b7dc27c5671ca2852095d08902
Anubhav saha <anubhavvsaha2001@gmail.com>
2021-11-26 03:24:11 +0530

    :building_construction: auth redux implementation

---
+++ src/actions/userActions.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL
+} from '../constants/userConstants';
+import URL from '../config';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const { data } = await axios.post(`${URL}/users/login`, { email, password }, config);
+    if (data.isAdmin) {
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: data
+      });
+
+      localStorage.setItem('userInfo', JSON.stringify(data));
+    } else {
+      dispatch({
+        type: USER_LOGIN_FAIL,
+        payload: 'Admin users only.'
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, number, password) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST
+    });
+
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    await axios.post(`${URL}/users/admin`, { name, email, number, password }, config);
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+};
